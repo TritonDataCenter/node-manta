TritonDataCenter/node-manta

commit 0da69a01e3ff750620276f74b27e3e277106398c

#30 signUrl not working with SSH agent; msign as well

lib/auth.js
@@ -140,11 +140,16 @@ function createSSHAgent(cb) {
     cb(null, agent);
 }
 
-function sshAgentSign(client, key, data, cb) {
+function sshAgentSign(client, key, data, alg, cb) {
     assert.object(client, 'sshAgentClient');
     assert.object(client._signCache, 'sshAgentClient');
     assert.object(key, 'key');
     assert.object(data, 'data (Buffer)');
+    if (typeof (alg) === 'function') {
+        cb = alg;
+        alg = 'rsa-sha1';
+    }
+    assert.string(alg, 'algorithm');
     assert.func(cb, 'callback');
     var c = client._signCache;
 
@@ -189,7 +194,7 @@ function sshAgentSign(client, key, data, cb) {
         }
 
         var sig = {
-            algorithm: 'rsa-sha1',
+            algorithm: alg,
             signature: signature.signature
         };
 
@@ -273,6 +278,7 @@ function cliSigner(options) {
     assert.string(options.keyId, 'options.keyId');
     assert.string(options.user, 'options.user');
 
+    var alg = options.algorithm ? options.algorithm.toLowerCase() : 'rsa-sha1';
     var initOpts = new EventEmitter();
     initOpts.setMaxListeners(1000);
     var keyId = options.keyId;
@@ -292,6 +298,11 @@ function cliSigner(options) {
                         return;
                     }
 
+                    if (alg !== 'rsa-sha1') {
+                        cb(new Error('SSH agent only supports RSA-SHA1'));
+                        return;
+                    }
+
                     opts.agent = agent;
                     cb();
                 });
@@ -305,9 +316,9 @@ function cliSigner(options) {
 
                 var a = opts.agent;
                 sshAgentGetKey(a, keyId, function (err, key) {
-                    if (!err) {
+                    if (!err)
                         opts.key = key;
-                    }
+
                     cb();
                 });
 
@@ -325,10 +336,14 @@ function cliSigner(options) {
                         return;
                     }
 
-                    var alg = / DSA /.test(key) ?
+                    if (alg && / DSA /.test(key) && /rsa/.test(alg)) {
+                        cb(new Error('RSA signing requested; DSA key loaded'));
+                        return;
+                    }
+
+                    opts.alg = opts.algorithm = alg || / DSA /.test(key) ?
                         'DSA-SHA1' :
                         'RSA-SHA256';
-                    opts.alg = opts.algorithm = alg;
                     opts.key = key;
                     cb();
                 });
@@ -374,17 +389,16 @@ function cliSigner(options) {
                         return;
                     }
 
-
                     var a = initOpts.agent;
                     var d = new Buffer(str);
                     var k = initOpts.key;
-                    sshAgentSign(a, k, d, function (e, s) {
+                    sshAgentSign(a, k, d, alg, function (e, s) {
                         if (e) {
                             cb(e);
                             return;
                         }
 
-                        s.algorithm = 'rsa-sha1';
+                        s.algorithm = alg;
                         s.keyId = keyId;
                         s.user = options.user;
                         opts.res = s;
@@ -460,17 +474,18 @@ function signUrl(opts, cb) {
     }).join('&');
     line += str;
 
-    if (opts.log) {
+    if (opts.log)
         opts.log.debug('signUrl: signing -->\n%s', line);
-    }
-    opts.sign(line, function (err, obj) {
-        if (err)
-            return (cb(err));
 
-        var u = opts.path + '?' + str + '&signature=' +
-            rfc3986(obj.signature);
-
-        return (cb(null, u));
+    opts.sign(line, function onSignature(err, obj) {
+        if (err) {
+            cb(err);
+        } else {
+            var u = opts.path + '?' +
+                str +
+                '&signature=' + rfc3986(obj.signature);
+            cb(null, u);
+        }
     });
 }
 

bin/mget
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 // -*- mode: js -*-
-// vim: set expandtab sw=4:
-// Copyright (c) 2012, Joyent, Inc. All rights reserved.
+// Copyright (c) 2013, Joyent, Inc. All rights reserved.
 
 var fs = require('fs');
 var path = require('path');
@@ -19,10 +18,10 @@ var manta = require('../lib');
 ///--- Globals
 
 var LOG = bunyan.createLogger({
-        name: path.basename(process.argv[1]),
-        level: (process.env.LOG_LEVEL || 'info'),
-        stream: process.stderr,
-        serializers: restify.bunyan.serializers
+    name: path.basename(process.argv[1]),
+    level: (process.env.LOG_LEVEL || 'info'),
+    stream: process.stderr,
+    serializers: restify.bunyan.serializers
 });
 
 var OPTIONS_PARSER = dashdash.createParser({
@@ -40,10 +39,10 @@ var OPTIONS_PARSER = dashdash.createParser({
 ///--- Functions
 
 function ifError(err) {
-        if (err) {
-                console.error(err.toString());
-                process.exit(1);
-        }
+    if (err) {
+        console.error(err.toString());
+        process.exit(1);
+    }
 }
 
 
@@ -72,75 +71,74 @@ function parseOptions() {
 
 
 function printEntry(obj) {
-        console.log('%j', obj);
+    console.log('%j', obj);
 }
 
 
 
 ///--- Mainline
-// Leading _ to avoid scope conflicts in functions
 
 (function main() {
-        var opts = parseOptions();
-        var client = manta.createBinClient(opts);
-        var i = -1;
-        var out = process.stdout;
-        var drawProgressBar = true;
+    var opts = parseOptions();
+    var client = manta.createBinClient(opts);
+    var i = -1;
+    var out = process.stdout;
+    var drawProgressBar = true;
 
-        function get() {
-                var p = opts.paths[++i];
-                if (!p) {
-                        client.close();
-                        return;
-                }
-
-                client.get(p, function (err, stream, res) {
-                        ifError(err);
-
-                        var bar;
-                        if (drawProgressBar) {
-                                var baropts = {
-                                        filename: p
-                                };
-                                var clen =
-                                        parseInt(res.headers['content-length'],
-                                                 10);
-                                if (clen >= 0) {
-                                        baropts.size = clen;
-                                } else {
-                                        baropts.nosize = true;
-                                }
-                                bar = new manta.ProgressBar(baropts);
-                        }
-
-                        stream.on('data', function (data) {
-                                if (bar)
-                                        bar.advance(data.length);
-                                out.write(data);
-                        });
-                        stream.on('end', function () {
-                                if (bar)
-                                        bar.end();
-                                get();
-                        });
-                });
+    function get() {
+        var p = opts.paths[++i];
+        if (!p) {
+            client.close();
+            return;
         }
 
-        if (opts.quiet || !Boolean(process.stderr.isTTY))
-                drawProgressBar = false;
+        client.get(p, function (err, stream, res) {
+            ifError(err);
 
-        if (opts.file) {
-                out = fs.createWriteStream(opts.file);
-                out.on('open', get.bind(this));
-        } else {
-                if (out.isTTY) {
-                        /*
-                         * If we're outputting to a TTY, then we don't
-                         * want to draw the progress bar as it will likely
-                         * clobber the output.
-                         */
-                        drawProgressBar = false;
+            var bar;
+            if (drawProgressBar) {
+                var baropts = {
+                    filename: p
+                };
+                var clen =
+                    parseInt(res.headers['content-length'],
+                             10);
+                if (clen >= 0) {
+                    baropts.size = clen;
+                } else {
+                    baropts.nosize = true;
                 }
+                bar = new manta.ProgressBar(baropts);
+            }
+
+            stream.on('data', function (data) {
+                if (bar)
+                    bar.advance(data.length);
+                out.write(data);
+            });
+            stream.on('end', function () {
+                if (bar)
+                    bar.end();
                 get();
+            });
+        });
+    }
+
+    if (opts.quiet || !Boolean(process.stderr.isTTY))
+        drawProgressBar = false;
+
+    if (opts.file) {
+        out = fs.createWriteStream(opts.file);
+        out.on('open', get.bind(this));
+    } else {
+        if (out.isTTY) {
+            /*
+             * If we're outputting to a TTY, then we don't
+             * want to draw the progress bar as it will likely
+             * clobber the output.
+             */
+            drawProgressBar = false;
         }
+        get();
+    }
 })();

bin/msign
@@ -31,8 +31,22 @@ var OPTIONS_PARSER = dashdash.createParser({
             names: ['expires', 'e'],
             type: 'positiveInteger',
             help: 'expiration time (epoch). Default is 1hr from now.',
-            'default': (new Date().getTime() / 1000) + 3600,
+            'default': Math.floor((new Date().getTime() / 1000) + 3600),
             helpArg: 'EXPIRATION'
+        },
+        {
+            names: ['file', 'f'],
+            type: 'string',
+            help: 'Use private key. Default is to try the SSH agent first ' +
+                ' then fallback to $HOME/.ssh/id_rsa)',
+            helpArg: 'PRIVATE_KEY'
+        },
+        {
+            names: ['algorithm', 'g'],
+            type: 'string',
+            help: 'Sign using algorithm. Default is RSA-SHA256',
+            'default': 'RSA-SHA1',
+            helpArg: 'PRIVATE_KEY'
         },
         {
             names: ['method', 'm'],
@@ -50,7 +64,7 @@ var OPTIONS_PARSER = dashdash.createParser({
 
 function ifError(err) {
     if (err) {
-        console.error(err.toString());
+        console.error('msign: ' + err.toString());
         process.exit(1);
     }
 }
@@ -91,36 +105,31 @@ function parseOptions() {
 ///--- Mainline
 
 (function main() {
-    var options = parseOptions();
+    var client;
+    var opts = parseOptions();
+    try {
+        client = manta.createBinClient(opts);
+    } catch (e) {
+        ifError(e);
+    }
 
-    manta.loadSSHKey(options.keyId, function (load_err, key) {
-        ifError(load_err);
-
-        var alg = / DSA /.test(key) ? 'DSA-SHA1' : 'RSA-SHA256';
-        var signer = manta.privateKeySigner({
-            key: key,
-            keyId: options.keyId,
-            log: options.log,
-            user: options.account
+    opts.paths.forEach(function (p) {
+        var _opts = {
+            algorithm: opts.algorithm,
+            expires: opts.expires,
+            host: url.parse(opts.url).host,
+            keyId: opts.keyId,
+            log: opts.log,
+            method: opts.method,
+            path: p,
+            sign: client.sign,
+            user: opts.account
+        };
+        client.signUrl(_opts, function (err, resource) {
+            ifError(err);
+            console.log(opts.url + resource);
         });
-
-        options.paths.forEach(function (p) {
-            var opts = {
-                algorithm: alg,
-                expires: options.expires,
-                host: url.parse(options.url).host,
-                keyId: options.keyId,
-                log: options.log,
-                method: options.method || 'GET',
-                path: p,
-                sign: signer,
-                user: options.account
-            };
-            manta.signUrl(opts, function (err, resource) {
-                ifError(err);
-                console.log(options.url + resource);
-            });
-        });
-
     });
+
+    client.close();
 })();

lib/client.js
@@ -1,4 +1,4 @@
-// Copyright 2012 Joyent, Inc.  All rights reserved.
+// Copyright 2013 Joyent, Inc.  All rights reserved.
 
 var EventEmitter = require('events').EventEmitter;
 var crypto = require('crypto');
@@ -16,6 +16,8 @@ var once = require('once');
 var restify = require('restify');
 var uuid = require('node-uuid');
 var vasync = require('vasync');
+
+var auth = require('./auth');
 
 
 
@@ -2020,6 +2022,12 @@ MantaClient.prototype.jobErrors = function getJobErrors(j, opts, cb) {
 
         return;
     });
+};
+
+
+// Just a proxy
+MantaClient.prototype.signUrl = function signUrl() {
+    auth.signUrl.apply(this, arguments);
 };
 
 

lib/create_client.js
@@ -102,7 +102,10 @@ function createClient(options) {
         });
     }
 
-    return (new manta.MantaClient(opts));
+    var client = new manta.MantaClient(opts);
+    client.signUrl = auth.signUrl;
+
+    return (client);
 }
 
 
@@ -162,6 +165,7 @@ function createBinClient(opts) {
     opts.sign = null;
     if (!opts.noAuth) {
         opts.sign = auth.cliSigner({
+            algorithm: opts.algorithm,
             keyId: opts.keyId,
             log: opts.log,
             user: opts.user

test/client.test.js
@@ -9,7 +9,7 @@ var uuid = require('node-uuid');
 var manta = require('../lib');
 
 if (require.cache[__dirname + '/helper.js'])
-        delete require.cache[__dirname + '/helper.js'];
+    delete require.cache[__dirname + '/helper.js'];
 var helper = require('./helper.js');
 
 
@@ -32,317 +32,318 @@ var CHILD2 = SUBDIR2 + '/' + uuid(); // link
 ///--- Tests
 
 before(function (cb) {
-        var f = process.env.SSH_KEY || process.env.HOME + '/.ssh/id_rsa';
-        var cmd = 'ssh-keygen -l -f ' +
-                f + ' ' +
-                '| awk \'{print $2}\'';
-        var self = this;
-        var url = process.env.MANTA_URL || 'http://localhost:8080';
-        var user = process.env.MANTA_USER || 'admin';
+    var f = process.env.SSH_KEY || process.env.HOME + '/.ssh/id_rsa';
+    var cmd = 'ssh-keygen -l -f ' +
+        f + ' ' +
+        '| awk \'{print $2}\'';
+    var self = this;
+    var url = process.env.MANTA_URL || 'http://localhost:8080';
+    var user = process.env.MANTA_USER || 'admin';
 
-        fs.readFile(f, 'utf8', function (err, key) {
-                if (err)
-                        return (cb(err));
+    fs.readFile(f, 'utf8', function (err, key) {
+        if (err)
+            return (cb(err));
 
-                exec(cmd, function (err2, stdout, stderr) {
-                        if (err2)
-                                return (cb(err2));
+        exec(cmd, function (err2, stdout, stderr) {
+            if (err2)
+                return (cb(err2));
 
-                        self.client = manta.createClient({
-                                connectTimeout: 1000,
-                                log: helper.createLogger(),
-                                retry: false,
-                                sign: manta.privateKeySigner({
-                                        key: key,
-                                        keyId: stdout.replace('\n', ''),
-                                        user: user
-                                }),
-                                url: url,
-                                user: user
-                        });
+            self.client = manta.createClient({
+                connectTimeout: 1000,
+                log: helper.createLogger(),
+                retry: false,
+                sign: manta.privateKeySigner({
+                    key: key,
+                    keyId: stdout.replace('\n', ''),
+                    user: user
+                }),
+                url: url,
+                user: user
+            });
 
-                        return (cb());
-                });
-
-                return (undefined);
+            return (cb());
         });
+
+        return (undefined);
+    });
 });
 
 
 after(function (cb) {
+    if (this.client)
         this.client.close();
-        cb();
+    cb();
 });
 
 
 test('mkdir', function (t) {
-        this.client.mkdir(SUBDIR1, function (err) {
-                t.ifError(err);
-                t.end();
-        });
+    this.client.mkdir(SUBDIR1, function (err) {
+        t.ifError(err);
+        t.end();
+    });
 });
 
 
 test('mkdir (sub)', function (t) {
-        this.client.mkdir(SUBDIR2, function (err) {
-                t.ifError(err);
-                t.end();
-        });
+    this.client.mkdir(SUBDIR2, function (err) {
+        t.ifError(err);
+        t.end();
+    });
 });
 
 
 test('put', function (t) {
-        var text = 'The lazy brown fox \nsomething \nsomething foo';
-        var size = Buffer.byteLength(text);
-        var stream = new MemoryStream();
+    var text = 'The lazy brown fox \nsomething \nsomething foo';
+    var size = Buffer.byteLength(text);
+    var stream = new MemoryStream();
 
-        this.client.put(CHILD1, stream, {size: size}, function (err) {
-                t.ifError(err);
-                t.end();
-        });
+    this.client.put(CHILD1, stream, {size: size}, function (err) {
+        t.ifError(err);
+        t.end();
+    });
 
-        process.nextTick(function () {
-                stream.write(text);
-                stream.end();
-        });
+    process.nextTick(function () {
+        stream.write(text);
+        stream.end();
+    });
 });
 
 
 test('put MD5 mismatch', function (t) {
-        var text = 'The lazy brown fox \nsomething \nsomething foo';
-        var size = Buffer.byteLength(text);
-        var opts = {
-                md5: new Buffer(text).toString('base64'),
-                size: size
-        };
-        var stream = new MemoryStream();
+    var text = 'The lazy brown fox \nsomething \nsomething foo';
+    var size = Buffer.byteLength(text);
+    var opts = {
+        md5: new Buffer(text).toString('base64'),
+        size: size
+    };
+    var stream = new MemoryStream();
 
-        this.client.put(CHILD1, stream, opts, function (err) {
-                t.ok(err);
-                t.end();
-        });
+    this.client.put(CHILD1, stream, opts, function (err) {
+        t.ok(err);
+        t.end();
+    });
 
-        process.nextTick(function () {
-                stream.write(text);
-                stream.end();
-        });
+    process.nextTick(function () {
+        stream.write(text);
+        stream.end();
+    });
 });
 
 
 test('ls', function (t) {
-        t.expect(5);
-        this.client.ls(SUBDIR1, function (err, res) {
-                t.ifError(err);
-                res.once('object', function (obj) {
-                        t.ok(obj);
-                        t.equal(obj.type, 'object');
-                });
-                res.once('directory', function (dir) {
-                        t.ok(dir);
-                        t.equal(dir.type, 'directory');
-                });
-                res.once('end', t.end.bind(t));
+    t.expect(5);
+    this.client.ls(SUBDIR1, function (err, res) {
+        t.ifError(err);
+        res.once('object', function (obj) {
+            t.ok(obj);
+            t.equal(obj.type, 'object');
         });
+        res.once('directory', function (dir) {
+            t.ok(dir);
+            t.equal(dir.type, 'directory');
+        });
+        res.once('end', t.end.bind(t));
+    });
 });
 
 
 test('ln', function (t) {
-        this.client.ln(CHILD1, CHILD2, function (err) {
-                t.ifError(err);
-                t.end();
-        });
+    this.client.ln(CHILD1, CHILD2, function (err) {
+        t.ifError(err);
+        t.end();
+    });
 });
 
 
 test('info (link)', function (t) {
-        this.client.info(CHILD2, function (err, type) {
-                t.ifError(err);
-                t.ok(type);
-                if (type) {
-                        t.equal(type.extension, 'bin');
-                        t.ok(type.size);
-                        t.equal(type.type, 'application/octet-stream');
-                        t.ok(type.etag);
-                        t.ok(type.md5);
-                }
-                t.end();
-        });
+    this.client.info(CHILD2, function (err, type) {
+        t.ifError(err);
+        t.ok(type);
+        if (type) {
+            t.equal(type.extension, 'bin');
+            t.ok(type.size);
+            t.equal(type.type, 'application/octet-stream');
+            t.ok(type.etag);
+            t.ok(type.md5);
+        }
+        t.end();
+    });
 });
 
 
 test('create job (simple grep)', function (t) {
-        var j = 'grep foo';
+    var j = 'grep foo';
 
-        this.client.createJob(j, function (err, job) {
-                t.ifError(err);
-                t.ok(job);
-                JOB = job;
-                t.end();
-        });
+    this.client.createJob(j, function (err, job) {
+        t.ifError(err);
+        t.ok(job);
+        JOB = job;
+        t.end();
+    });
 });
 
 
 test('get job', function (t) {
-        this.client.job(JOB, function (err, job) {
-                t.ifError(err);
-                t.ok(job);
-                t.equal(job.id, JOB);
-                t.ok(job.name);
-                t.ok((job.state === 'queued' || job.state === 'running'));
-                t.ok(job.timeCreated);
-                t.ok(job.phases);
-                t.notOk(job.cancelled);
-                t.notOk(job.inputDone);
-                t.end();
-        });
+    this.client.job(JOB, function (err, job) {
+        t.ifError(err);
+        t.ok(job);
+        t.equal(job.id, JOB);
+        t.ok(job.name);
+        t.ok((job.state === 'queued' || job.state === 'running'));
+        t.ok(job.timeCreated);
+        t.ok(job.phases);
+        t.notOk(job.cancelled);
+        t.notOk(job.inputDone);
+        t.end();
+    });
 });
 
 
 test('add input keys', function (t) {
-        var keys = [
-                CHILD1,
-                CHILD2
-        ];
+    var keys = [
+        CHILD1,
+        CHILD2
+    ];
 
-        this.client.addJobKey(JOB, keys, function (err) {
-                t.ifError(err);
-                t.end();
-        });
+    this.client.addJobKey(JOB, keys, function (err) {
+        t.ifError(err);
+        t.end();
+    });
 });
 
 
 test('get job input', function (t) {
-        var keys = 0;
-        function cb(err) {
-                t.ifError(err);
-                t.equal(keys, 2);
-                t.end();
-        }
+    var keys = 0;
+    function cb(err) {
+        t.ifError(err);
+        t.equal(keys, 2);
+        t.end();
+    }
 
-        this.client.jobInput(JOB, function (err, res) {
-                t.ifError(err);
-                t.ok(res);
+    this.client.jobInput(JOB, function (err, res) {
+        t.ifError(err);
+        t.ok(res);
 
-                res.on('key', function (k) {
-                        t.ok(k);
-                        keys++;
-                });
-
-                res.once('error', cb);
-                res.once('end', cb.bind(null, null));
+        res.on('key', function (k) {
+            t.ok(k);
+            keys++;
         });
+
+        res.once('error', cb);
+        res.once('end', cb.bind(null, null));
+    });
 });
 
 
 test('end job', function (t) {
-        this.client.endJob(JOB, function (err) {
-                t.ifError(err);
-                t.end();
-        });
+    this.client.endJob(JOB, function (err) {
+        t.ifError(err);
+        t.end();
+    });
 });
 
 
 test('wait for job', function (t) {
-        var attempts = 1;
-        var client = this.client;
+    var attempts = 1;
+    var client = this.client;
 
-        function getState() {
-                client.job(JOB, function (err, job) {
-                        t.ifError(err);
-                        if (err) {
-                                t.end();
-                        } else if (job.state === 'done') {
-                                t.end();
-                        } else {
-                                if (++attempts >= 60) {
-                                        t.notOk(attempts);
-                                        t.end();
-                                } else {
-                                        setTimeout(getState, 1000);
-                                }
-                        }
-                });
-        }
+    function getState() {
+        client.job(JOB, function (err, job) {
+            t.ifError(err);
+            if (err) {
+                t.end();
+            } else if (job.state === 'done') {
+                t.end();
+            } else {
+                if (++attempts >= 60) {
+                    t.notOk(attempts);
+                    t.end();
+                } else {
+                    setTimeout(getState, 1000);
+                }
+            }
+        });
+    }
 
-        getState();
+    getState();
 });
 
 
 test('get job output', function (t) {
-        var _keys = 0; // treat 'end' as a key
-        function cb(err) {
-                t.ifError(err);
-                t.ok(_keys > 0);
-                t.end();
-        }
+    var _keys = 0; // treat 'end' as a key
+    function cb(err) {
+        t.ifError(err);
+        t.ok(_keys > 0);
+        t.end();
+    }
 
-        this.client.jobOutput(JOB, function (err, res) {
-                t.ifError(err);
-                t.ok(res);
+    this.client.jobOutput(JOB, function (err, res) {
+        t.ifError(err);
+        t.ok(res);
 
-                res.on('key', function (k) {
-                        t.ok(k);
-                        _keys++;
-                });
-
-                res.once('error', cb);
-                res.once('end', cb.bind(null, null));
+        res.on('key', function (k) {
+            t.ok(k);
+            _keys++;
         });
+
+        res.once('error', cb);
+        res.once('end', cb.bind(null, null));
+    });
 });
 
 
 test('create and cancel job', function (t) {
-        var self = this;
+    var self = this;
 
-        this.client.createJob('grep foo', function (err, job) {
-                t.ifError(err);
-                t.ok(job);
-                self.client.cancelJob(job, function (err2) {
-                        t.ifError(err2);
-                        self.client.job(job, function (err3, job2) {
-                                t.ifError(err3);
-                                t.ok(job2);
-                                t.ok(job2.cancelled);
-                                t.ok(job2.inputDone);
-                                t.equal(job2.state, 'done');
-                                t.end();
-                        });
-                });
+    this.client.createJob('grep foo', function (err, job) {
+        t.ifError(err);
+        t.ok(job);
+        self.client.cancelJob(job, function (err2) {
+            t.ifError(err2);
+            self.client.job(job, function (err3, job2) {
+                t.ifError(err3);
+                t.ok(job2);
+                t.ok(job2.cancelled);
+                t.ok(job2.inputDone);
+                t.equal(job2.state, 'done');
+                t.end();
+            });
         });
+    });
 });
 
 
 test('unlink object', function (t) {
-        this.client.unlink(CHILD2, function (err) {
-                t.ifError(err);
-                t.end();
-        });
+    this.client.unlink(CHILD2, function (err) {
+        t.ifError(err);
+        t.end();
+    });
 });
 
 
 test('unlink link', function (t) {
-        this.client.unlink(CHILD1, function (err) {
-                t.ifError(err);
-                t.end();
-        });
+    this.client.unlink(CHILD1, function (err) {
+        t.ifError(err);
+        t.end();
+    });
 });
 
 
 test('rmr', function (t) {
-        this.client.rmr(SUBDIR1, function (err) {
-                t.ifError(err);
-                t.end();
-        });
+    this.client.rmr(SUBDIR1, function (err) {
+        t.ifError(err);
+        t.end();
+    });
 });
 
 
 test('mkdirp/rmr', function (t) {
-        var self = this;
-        this.client.mkdirp(SUBDIR2, function (err) {
-                t.ifError(err);
-                self.client.rmr(SUBDIR1, function (err2) {
-                        t.ifError(err2);
-                        t.end();
-                });
+    var self = this;
+    this.client.mkdirp(SUBDIR2, function (err) {
+        t.ifError(err);
+        self.client.rmr(SUBDIR1, function (err2) {
+            t.ifError(err2);
+            t.end();
         });
+    });
 });

commit 17e4f0b66d19c68a8d2f6bdf5ad08aea9efd2035

#0da69a0 broke non-SSH agent signing

lib/auth.js
@@ -341,9 +341,9 @@ function cliSigner(options) {
                         return;
                     }
 
-                    opts.alg = opts.algorithm = alg || / DSA /.test(key) ?
+                    opts.alg = opts.algorithm = alg || (/ DSA /.test(key) ?
                         'DSA-SHA1' :
-                        'RSA-SHA256';
+                        'RSA-SHA256');
                     opts.key = key;
                     cb();
                 });
@@ -412,7 +412,8 @@ function cliSigner(options) {
                         return;
                     }
 
-                    var a = initOpts.algorithm;
+
+                    var a = initOpts.algorithm.toUpperCase();
                     var k = initOpts.key;
                     var s = crypto.createSign(a);
                     s.update(str);