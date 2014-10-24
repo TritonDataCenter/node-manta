7fd9d8ddc9e68e5532fc720cb3f80370cc61259e
Trent Mick <trentm@gmail.com>
2014-10-23 17:03:46 -0700

    add a quick example of how to get manta client-side trace logging

---
+++ examples/client-trace-logging.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+/**
+ * Example node script showing how to get trace-level logging of the
+ * *client-side* of talking to Manta.
+ *
+ * Usage:
+ *      git clone git@github.com:joyent/node-manta.git
+ *      cd node-manta
+ *      npm install
+ *      node examples/client-trace-logging.js | bunyan
+ */
+
+var bunyan = require('bunyan');
+var manta = require('../');
+
+
+var log = bunyan.createLogger({
+    name: 'client-trace-logging',
+    level: 'trace',   // <---- set log level here
+    serializers: bunyan.stdSerializers
+});
+
+
+
+var client = manta.createBinClient({log: log});
+
+client.ls('~~/stor', function (err, results) {
+    if (err) {
+        log.error(err, 'ls failed');
+        process.exit(1);
+    }
+
+    results.on('error', function (err) {
+        log.error(err, 'ls results error');
+    });
+    results.on('entry', function (dirent) {
+        log.info({dirent: dirent}, 'entry');
+    });
+    results.on('end', function () {
+        log.info('end');
+        process.exit(0);
+    });
+});
